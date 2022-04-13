63fa3bbde481235b1e4a13c3c5cf8b56794deb79
BP_Samveg Shah <bp_samveg_shah@colpal.com>
2022-04-13 10:01:26 +0530

    changes

--- validator/Test/forms.py
+++ validator/Test/forms.py
@@ -6,6 +6,33 @@ class Credentials(forms.Form):
     schema = forms.CharField(label='Your schema', max_length=100)
     database = forms.CharField(label='Your database', max_length=100)
     table = forms.CharField(label='Your table', max_length=100)
+    num_level = forms.IntegerField(label='Enter number of levels')
+    num_kpis = forms.IntegerField(label='Enter number of kpis')
+    num_filter = forms.IntegerField(label='Enter number of filter parameters')
+
+class Level(forms.Form):
+    levels = forms.ChoiceField(choices=())
+    def __init__(self, *args, **kwargs):
+        levels_choices = kwargs.pop('levels_choices', ())
+        super().__init__(*args, **kwargs)
+        self.fields['levels'].choices = levels_choices
+
+class KPIs(forms.Form):
+    kpi=forms.ChoiceField(choices=())
+    aggregation=forms.CharField(label='Enter aggregation')
+    def __init__(self,kpi_choices, *args, **kwargs):
+        kpi_choices = kwargs.pop('kpi_choices', ())
+        super().__init__(*args, **kwargs)
+        self.fields['kpi'].choices = kpi_choices
+
+class Filter(forms.Form):    
+    filter_parameter=forms.ChoiceField(choices=())
+    filter_criteria=forms.CharField(label='Enter criteria')
+    filter_value=forms.CharField(label='Enter value')
+    def __init__(self, *args, **kwargs):
+        filter_parameter_choices = kwargs.pop('filter_parameter_choices', ())
+        super().__init__(*args, **kwargs)
+        self.fields['filter_parameter'].choices = filter_parameter_choices
 
 
 
--- validator/Test/views.py
+++ validator/Test/views.py
@@ -1,36 +1,131 @@
+from random import choices
+from ssl import ALERT_DESCRIPTION_ACCESS_DENIED
+from django.forms import modelform_factory
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from calendar import c
+from django.forms import formset_factory
+
 from filecmp import cmp
 import pandas as pd
 import snowflake.connector
 # import config
+from django.contrib import messages
 
-from .forms import Credentials
-
+from .forms import Credentials,Filter,KPIs,Level
+num_level=0
+num_kpis=0
+num_filter=0
+final=[]
 def index(request):
+    global num_level
+    global num_kpis
+    global num_filter
+    global final
+    Credentialsset=formset_factory(Credentials,extra=1)
     if request.method ==  "POST":
-        form = Credentials(request.POST)
-        print(form)
-        if form.is_valid():
-            print("HITUZhfb")
-            username = form.cleaned_data['username']
-            print(username)
-            password = form.cleaned_data['password']
-            print(password)
-            schema = form.cleaned_data['schema']
-            print(schema)
-            database = form.cleaned_data['database']
-            print(database)
-            table = form.cleaned_data['table']
+        # form = Credentials(request.POST)
+        formi = Credentialsset(request.POST)
+        # print(form)
+        if formi.is_valid():
+            for f in formi: 
+                cd = f.cleaned_data
+                username = cd.get('username')
+                passwordd = cd.get('password')
+                schemaa = cd.get('schema')
+                databasee=cd.get('database')
+                table = cd.get('table')
+                num_level=cd.get('num_level')
+                num_filter=cd.get('num_filter')
+                num_kpis=cd.get('num_kpis')
+            print(passwordd)
+            print(schemaa)
+            print(databasee)
             print(table)
+            print(num_level)
+            print(num_kpis)
+            print(num_filter)
+
+            try:
+                con = snowflake.connector.connect(
+                    user=username,
+                    account = "colgatepalmolivedev.us-central1.gcp",
+                    authenticator = "externalbrowser",
+                    password= passwordd,
+                    warehouse = "DEVELOPER_WH",
+                    database= databasee,
+                    schema= schemaa,
+                )
+                cur = con.cursor()
+
+                # STEP 2: Displaying Preview of Table
+
+                preview_query = "SELECT * FROM " + table + " LIMIT 10 "
+                # print(preview_query)
+                cur.execute(preview_query)
+                columns = []
+                for val in cur.description:
+                    columns.append(val[0])
+                rows = 0
+                while True:
+                    # print("Inside while")
+                    dat = cur.fetchall()
+                    if not dat:
+                        break
+                    # print(cur.description)
+                    df = pd.DataFrame(dat, columns=columns)
+                    result=df.columns.values.tolist()
+                    
+                    
+                    
+        
+
+                    rows += df.shape[0]
+            except:
+                print("error")
+                messages.info(request,"Wrong credentails")
+                return redirect('/Test')
+
+
+            # print(result)
+            for i in range(1,len(result)+1):
+                z=[]
+                z.append(str(i))
+                z.append(str(result[i-1]))
+                z=tuple(z)
+                final.append(z)
+            final=tuple(final)
+            df.to_csv('SF_PREVIEW.csv', index=False)
+
+            print("PREVIEW OF TABLE SAVED IN SF_PREVIEW.csv")
             # mail(Name,email,Country)
             return redirect('detail')
         return redirect('/Test')
-    form = Credentials()
-    # return render(request,"country/register.html",{"form":form})
-    return render(request,"Test/index.html",{"form":form})
+    
+    return render(request,"Test/index.html",{"form":Credentialsset})
 
 def detail(request):
-    
-    return render(request,"Test/detail.html")
+    global num_level
+    global num_kpis
+    global num_filter
+    global final
+    form1=Filter(choices=final)
+    Filterset=formset_factory(form1,extra=num_filter)
+    # KPIset=formset_factory(KPIs(choices=final),extra=num_kpis)
+    # Levelset=formset_factory(Level(choices=final),extra=num_filter)
+
+
+
+
+
+    return render(request,"Test/detail.html",{"form1":Filterset})
+    # if request.method ==  "POST":
+    #     form = Credentials(request.POST)
+    #     print(form)
+    #     if form.is_valid():
+    #         print("hey")
+            
+    # else:
+    #     redentialsset=formset_factory(Credentials,extra=2)
+
+    #     return render(request,"Test/detail.html")
