740971347a11b606fa5387d266dd3cbb40b1d1f9
BP_Shantanu Godbole <bp_shantanu_godbole@colpal.com>
2022-04-21 11:00:03 +0530

    added basic loading indicator

--- validator/validator/urls.py
+++ validator/validator/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
+from . import settings
+from django.contrib.staticfiles.urls import static
 
 urlpatterns = [
     path('Test/', include('Test.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
